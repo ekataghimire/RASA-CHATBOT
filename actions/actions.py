8615e74edd0c17c1add81a625f46f735875db604
ekataghac <ekata.ghimire@anchorpoints.com.np>
2023-04-26 07:39:35 +0545

    RASA 2nd Training

--- actions/actions.py
+++ actions/actions.py
@@ -7,11 +7,11 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
 #
 # class ActionHelloWorld(Action):
 #
@@ -25,3 +25,34 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+class ActionUtterSympathy(Action):
+    def name(self) -> Text:
+        return "action_utter_sympathy"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("I'm sorry to hear that. It must be really difficult to deal with depression symptoms. Please know that I'm here to support you in any way I can.")
+        return []
+
+class ActionUtterRecommendHelp(Action):
+    def name(self) -> Text:
+        return "action_utter_recommend_help"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("It's important to prioritize your mental health. I would recommend seeking professional help from a mental health professional. You can also check out online resources like the National Alliance on Mental Illness (NAMI) or the Substance Abuse and Mental Health Services Administration (SAMHSA).")
+        return []
+    
+class ActionUtterAcknowledgeSuicidal(Action):
+    def name(self) -> Text:
+        return "action_utter_acknowledge_suicidal"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("There is always a way out. Sucicide is not an option. Suicide hotline: 9898786786876876")
+        return []
+    
+class ActionUtterConnect_Crisis_Hotline(Action):
+    def name(self) -> Text:
+        return "action_utter_connect_crisis_hotline"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message("Start living life for yourself not others. YOU are the most important person to you. Self love is the biggest love. Suicide Prevention Hotline: 982373736372")
+        return []
